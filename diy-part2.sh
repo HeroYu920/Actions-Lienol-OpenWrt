b0862a81b1f48a153b2ec0c33322ea89ffa09598
lxhao61 <lxhao61@gmail.com>
2022-02-21 00:15:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -40,10 +40,10 @@ rm -rf feeds/packages/net/softethervpn
 svn co https://github.com/lxhao61/openwrt-packages/trunk/net/softethervpn feeds/packages/net/softethervpn
 
 # 删除原版softethervpn5插件
-rm -rf feeds/packages/net/softethervpn5
+#rm -rf feeds/packages/net/softethervpn5
 
 # 拉取Lean大softethervpn5插件
-svn co https://github.com/coolsnowwolf/packages/trunk/net/softethervpn5 feeds/packages/net/softethervpn5
+#svn co https://github.com/coolsnowwolf/packages/trunk/net/softethervpn5 feeds/packages/net/softethervpn5
 
 # 删除原版luci-app-softethervpn插件
 rm -rf feeds/lienol/luci-app-softethervpn
