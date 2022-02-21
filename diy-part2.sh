5913e8da948bd8879fe78f39e7ccd27fc605ad0f
lxhao61 <lxhao61@gmail.com>
2022-02-22 01:20:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,10 +25,10 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone https://github.com/tungnt017/luci-app-ipsec-vpnd package/lean/luci-app-ipsec-vpnd
 
 # 删除原版softethervpn插件
-rm -rf feeds/packages/net/softethervpn
+#rm -rf feeds/packages/net/softethervpn
 
 # 拉取lienol大softethervpn插件
-svn co https://github.com/lxhao61/openwrt-packages/trunk/net/softethervpn feeds/packages/net/softethervpn
+#svn co https://github.com/lxhao61/openwrt-packages/trunk/net/softethervpn feeds/packages/net/softethervpn
 
 # 删除原版luci-app-softethervpn插件
 #rm -rf feeds/lienol/luci-app-softethervpn
